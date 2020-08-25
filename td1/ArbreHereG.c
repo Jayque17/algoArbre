e9dd834f1c7558998e60a9312e107c9843f40f7d
Julien Noel <julien.leon99@gmail.com>
2020-08-25 10:40:19 +0200

    td1 almost finished

--- td1/ArbreHereG.c
+++ td1/ArbreHereG.c
@@ -1,4 +1,14 @@
-/*
+#include <stdio.h>
+
+typedef struct noeud{
+    
+    int val; /* étiquette */
+    struct noeud *fg; /* fils gauche */
+    struct noeud *fd; /* fils droit */
+
+} Noeud, *Arbre;
+
+/* a.
       0
      / \
     0   0
@@ -32,3 +42,27 @@
 0   0
 
 */
+
+/* b. */
+
+int estHG(Arbre a){
+
+      Noeud* node = a;
+
+      if(node->fd == NULL && node->fg == NULL){
+
+            return 1;
+      }
+      else if(node->fd != NULL && node->fg == NULL){
+
+            return 0;
+      }
+      else if(node->fd == NULL && node->fg != NULL){
+
+            estHG(node->fg);
+      }
+
+      estHG(node->fd);
+      estHG(node->fg);
+
+}
